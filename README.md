f29467088d6095eba0892dba1c005089f01139af
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-05-10 17:11:07 +0200

    add license, readme and package.json

---
+++ README.md
@@ -0,0 +1,8 @@
+# eslint-config-goreutils
+
+This is the reusable [eslint](http://eslint.org/) config for [goreutils](https://github.com/goreutils) projects.
+
+## See also:
+
+* http://eslint.org/docs/developer-guide/shareable-configs
+* https://github.com/goreutils/gore-gulp
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-{
+/**
+ * Copyright (c) 2015-present, goreutils
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+"use strict";
+
+module.exports = {
     "ecmaFeatures": {
         "arrowFunctions": true,
         "blockBindings": true,
@@ -187,4 +197,4 @@
         "wrap-regex": 2,
         "yoda": 0
     }
-}
+};
